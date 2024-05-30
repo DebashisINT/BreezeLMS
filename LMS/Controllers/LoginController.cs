8399ddd3b4d1998e5a21302998e90f66f2279775
PritiINT <62432969+PritiINT@users.noreply.github.com>
2024-05-30 13:33:37 +0530

    check in

---
+++ LMS/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using LMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace LMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        private IConfiguration Configuration;
+        public LoginController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        private string GetConnectionString()
+        {
+            return this.Configuration.GetConnectionString("DefaultConnection");
+        }
+        public ActionResult SubmitForm(LoginModel omodel)
+        {
+            using (SqlConnection con = new SqlConnection(GetConnectionString()))
+            {
+                string Encryptpass = omodel.Encrypt(omodel.password.Trim());
+
+                DataTable ds = new DataTable();
+                using (SqlCommand cmd = new SqlCommand("PRC_LMSLOGIN", con))
+                {
+                    cmd.Parameters.AddWithValue("@ACTION", "LOGIN");
+                    cmd.Parameters.AddWithValue("@username", omodel.username);
+                    cmd.Parameters.AddWithValue("@password", Encryptpass);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = 0;
+                    SqlDataAdapter Adap = new SqlDataAdapter();
+                    Adap.SelectCommand = cmd;
+                    Adap.Fill(ds);
+                    cmd.Dispose();
+                    con.Dispose();
+
+                    if (ds != null && ds.Rows.Count > 0)
+                    {
+                        return RedirectToAction("Privacy", "Home");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Login");
+                    }                    
+                }
+            }
+
+            // return View();
+
+        }
+
+    }
+}
---
+++ LMS/Models/LoginModel.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace LMS.Models
+{
+    public class LoginModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+
+        private string Password = "3269875";
+        private string Salt = "05983654";
+        private string HashAlgorithm = "SHA1";
+        private int PasswordIterations = 2;
+        private string InitialVector = "OFRna73m*aze01xY";
+        private int KeySize = 256;
+
+        public string _password
+        {
+            get { return Password; }
+        }
+
+        public string salt
+        {
+            get { return Salt; }
+        }
+
+        public string hashAlgo
+        {
+            get { return HashAlgorithm; }
+        }
+
+        public int passwordterations
+        {
+            get { return PasswordIterations; }
+        }
+
+        public string initialvector
+        {
+            get { return InitialVector; }
+        }
+
+        public int keysize
+        {
+            get { return KeySize; }
+        }
+        public string Encrypt(string PlainText)
+        {
+            if (string.IsNullOrEmpty(PlainText))
+                return "";
+            byte[] InitialVectorBytes = Encoding.ASCII.GetBytes(initialvector);
+            byte[] SaltValueBytes = Encoding.ASCII.GetBytes(salt);
+            byte[] PlainTextBytes = Encoding.UTF8.GetBytes(PlainText);
+            PasswordDeriveBytes DerivedPassword = new PasswordDeriveBytes(_password, SaltValueBytes, hashAlgo, passwordterations);
+            byte[] KeyBytes = DerivedPassword.GetBytes(KeySize / 8);
+            RijndaelManaged SymmetricKey = new RijndaelManaged();
+            SymmetricKey.Mode = CipherMode.CBC;
+            byte[] CipherTextBytes = null;
+            using (ICryptoTransform Encryptor = SymmetricKey.CreateEncryptor(KeyBytes, InitialVectorBytes))
+            {
+                using (MemoryStream MemStream = new MemoryStream())
+                {
+                    using (CryptoStream CryptoStream = new CryptoStream(MemStream, Encryptor, CryptoStreamMode.Write))
+                    {
+                        CryptoStream.Write(PlainTextBytes, 0, PlainTextBytes.Length);
+                        CryptoStream.FlushFinalBlock();
+                        CipherTextBytes = MemStream.ToArray();
+                        MemStream.Close();
+                        CryptoStream.Close();
+                    }
+                }
+            }
+            SymmetricKey.Clear();
+            return Convert.ToBase64String(CipherTextBytes);
+        }
+    }
+}
